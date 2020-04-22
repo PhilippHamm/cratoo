1d086cada81d2c7ac54a76893e2f2546a50aa08e
Beixi Lu <beixi_lu@yahoo.com>
2020-04-22 22:12:56 +0200

    modles

---
+++ db/migrate/20200422185124_create_styles.rb
@@ -0,0 +1,10 @@
+class CreateStyles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :styles do |t|
+      t.string :name
+      t.references :genre, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
