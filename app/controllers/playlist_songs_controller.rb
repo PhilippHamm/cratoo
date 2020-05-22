a85a7edb68583c0b10071a913767587b65db5fcc
Philipp Hamm <philipp.hamm@gmail.com>
2020-05-22 15:49:58 +0200

    mvp tinder

--- app/controllers/playlist_songs_controller.rb
+++ app/controllers/playlist_songs_controller.rb
@@ -1,2 +1,19 @@
 class PlaylistSongsController < ApplicationController
+  def create
+    if Playlist.where(user_id: current_user.id).count == 1
+      likes = Playlist.new(name: "Likes", user_id: current_user.id)
+      likes.save
+      dislikes = Playlist.new(name: "Dislikes", user_id: current_user.id)
+      dislikes.save
+    end
+    likes = Playlist.find_by(name: "Likes", user_id: current_user.id)
+    dislikes = Playlist.find_by(name: "Dislikes", user_id: current_user.id)
+    if params[:status] == "like"
+      user_choice = PlaylistSong.new(playlist_id: likes.id, song_id: params[:song_id].to_i)
+    else
+      user_choice = PlaylistSong.new(playlist_id: dislikes.id, song_id: params[:song_id].to_i)
+    end
+    user_choice.save
+    redirect_to suggestion_songs_path
+  end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,5 @@
 class SongsController < ApplicationController
+  def suggestion
+    @suggestion = Song.all.sample
+  end
 end
